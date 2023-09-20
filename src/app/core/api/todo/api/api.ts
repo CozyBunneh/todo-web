328e6efe439724b4df0501664742974f45795e03
Julia Mattjus <julia.mattjus@gmail.com>
2023-09-20 11:00:57 +0200

    add the autogenerated api client and types

---
+++ src/app/core/api/todo/api/api.ts
@@ -0,0 +1,5 @@
+export * from './microProfileHealth.service';
+import { MicroProfileHealthService } from './microProfileHealth.service';
+export * from './todoResource.service';
+import { TodoResourceService } from './todoResource.service';
+export const APIS = [MicroProfileHealthService, TodoResourceService];
---
+++ src/app/core/api/todo/model/models.ts
@@ -0,0 +1,6 @@
+export * from './createTodoV1';
+export * from './healthCheckResponse';
+export * from './healthCheckStatus';
+export * from './paginationResponseV1TodoV1';
+export * from './priorityV1';
+export * from './todoV1';
